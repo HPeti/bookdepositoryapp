20113158762e8d267445058b07f86e659813c5c7
Péter Hegedűs <hpeti0912@gmail.com>
2021-12-10 17:52:22 +0100

    Created interface for Book

---
+++ src/app/book.ts
@@ -0,0 +1,10 @@
+export interface Book {
+    id: number;
+    author: string;
+    name: string;
+    category: string;
+    publisher: string;
+    price: number;
+    releaseDate: string;
+    bookCode: string;
+};
