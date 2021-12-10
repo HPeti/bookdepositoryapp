e85be2e15ac9a24f866cead534f1a3155cfcb7d1
Péter Hegedűs <hpeti0912@gmail.com>
2021-12-10 17:54:33 +0100

    Added basic layout and add book function.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Book } from './book';
+import { BookService } from './book.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bookdepositoryapp';
+export class AppComponent implements OnInit {
+  public books: Book[] = [];
+  public editBook: Book;
+  public deleteBook: Book;
+
+  constructor(private bookService: BookService){ }
+
+  ngOnInit(){
+    this.getBooks();
+  }
+
+  public getBooks(): void{
+    this.bookService.getBooks().subscribe(
+      (response: Book[]) => {
+        this.books = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onAddBook(addForm: NgForm): void{
+    this.bookService.addBook(addForm.value).subscribe(
+      (response: Book) => {
+        console.log(response);
+        this.getBooks();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
+  public onDeleteBook(bookId: number): void {
+    this.bookService.deleteBook(bookId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getBooks();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onOpenModal(book: Book | null, mode: string) : void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addBookModal');
+    }
+    else if (mode === 'add'){
+      button.setAttribute('data-target', '#updateBookModal');
+    }
+    else if (mode === 'delete'){
+      button.setAttribute('data-target', '#deleteBookModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+
 }
